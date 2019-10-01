f7d46a38918052a6cbd31fe474262c826b6dcd54
Vu Cong Anh <conganh270197@gmail.com>
2019-10-01 17:33:47 +0700

    Ceil&&Floor

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -4,12 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication1
+namespace ConsoleApplication2
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+
+        }
+        /*
+         *input : float x
+         *output : int a that is smallest but not smaller than x
+         *idea: a =(int)x then a=a+1
+         *if x is negative then a is unchanged
+         *if x ==a then a is unchanged
+         */
+        public static int Ceil(float x)
+        {
+            int a = (int)x;
+            if (x > 0)
+                if (x > a)
+                    a = a + 1;
+            return a;
+        }
+        /*
+         * intput floatx
+         * output int a that is largest but not bigger than x
+         * idead: a = (int)x
+         * if x is negative then a =a-1
+         */
+        public static int Floor(float x)
+        {
+            int a = (int)x;
+            if (x < 0 && x != a)
+                a = a - 1;
+
+            return a;
         }
     }
 }
